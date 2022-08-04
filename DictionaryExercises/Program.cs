58c76fddb617466f2327de649dd3df62d79ba877
narhiril <roxannarusbarsky@yahoo.com>
2022-08-04 14:41:02 -0500

    Dictionary exercises

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -6,7 +7,49 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            string input, name;
+
+            do
+            {
+                Console.Write("Enter a student ID number, or press ENTER to quit: ");
+                input = Console.ReadLine();
+                try
+                {
+                    do
+                    {
+                        Console.Write($"Enter student #{int.Parse(input)}'s name: ");
+                        name = Console.ReadLine();
+                        if (Equals(name, ""))
+                        {
+                            Console.WriteLine("Error: Name cannot be blank!");
+                        }
+                        else
+                        {
+                            students.Add(int.Parse(input), name);
+                            Console.WriteLine($"Student #{int.Parse(input)} ({name}) added!");
+                            break;
+                        }
+                    } while(true);
+                }
+                catch (FormatException e)
+                {
+                    if (Equals(input, ""))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: invalid ID number.");
+                    }
+                }
+            } while(true);
+
+            Console.WriteLine($"\nClass roster ({students.Count} students):");
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine($"Student #{student.Key}: {student.Value}");
+            }
         }
     }
 }
