faf63b77698c69e643e2b9064613293019345edf
narhiril <roxannarusbarsky@yahoo.com>
2022-08-04 13:32:44 -0500

    string exercise

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,18 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            Console.WriteLine("Practice String:");
+            string practiceString = 
+                "I would not, could not, in a box. " +
+                "I would not, could not with a fox. " +
+                "I will not eat them in a house. " +
+                "I will not eat them with a mouse.";
+            string[] practiceWords = practiceString.Split(' ');
+            Console.WriteLine(string.Join(" ", practiceWords));
+            Console.WriteLine("Break By Sentences:");
+            string[] practiceSentences = practiceString.Split(". ");
+            Console.WriteLine(string.Join(".\n", practiceSentences));
+
         }
     }
 }
