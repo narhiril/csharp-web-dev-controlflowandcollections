591d88977ef3fe00fbdf323fa0481b812816c6a4
narhiril <roxannarusbarsky@yahoo.com>
2022-08-04 13:21:20 -0500

    array exercise

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,20 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            Console.WriteLine("Practice Array:");
+            int[] practiceArray = { 1, 1, 2, 3, 5, 8 };
+            foreach (int i in practiceArray)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\nOdd numbers only:");
+            foreach (int i in practiceArray)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
